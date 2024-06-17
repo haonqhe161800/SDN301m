bc7b056e9f3efea71b19e3eea47d4c51c23c1f08
thetoannn <naotehtgnoud@gmail.com>
2024-06-17 10:13:21 +0700

    FE2 HR Company

--- FE/g1cv/src/routing/Router.jsx
+++ FE/g1cv/src/routing/Router.jsx
@@ -9,15 +9,10 @@ import Home from '../Components/Screens/Home';
 import NotFound from '../Components/Screens/NotFound';
 import LoginForCompany from '../Components/Screens/LoginForCompany';
 import CompanyRegister from '../Components/Screens/CompanyRegister';
-import AuthenCompany from '../Components/AuthenCompany';
 import VIPRegister from '../Components/Screens/VIPRegister';
-import AuthenStaff from '../Components/AuthenStaff';
-import ChatWithHR from '../Components/Screens/ChatWithHR';
 import EditPostHR from '../Components/Screens/EditPostHR';
 import LoginForStaff from '../Components/Screens/LoginForStaff';
-import AuthenHRManager from '../Components/AuthenHRManager';
 import HRManagerCandidatesList from '../Components/Screens/HRManagerCandidatesList';
-import ViewCV from '../Components/Screens/ViewCV';
 import CandidatesListHR from '../Components/Screens/CandidatesListHR';
 import JobListHR from '../Components/Screens/JobListHR';
 const Router = () => {
@@ -30,20 +25,18 @@ const Router = () => {
             <Route path='/khong-xac-dinh' element={<NotFound />} />
             <Route path='/quen-mat-khau/:code' element={<ForgotPassword />} />
             <Route path='/tao-bai-tuyen-dung' element={<RecruimentPostHR />} />
-            <Route element={<AuthenCompany />}>
+            <Route>
                 <Route path='/ho-so-cong-ty' element={<CompanyProfile />} />
                 <Route path='/danh-sach-nhan-vien' element={<ListEmployees />} />
                 <Route path='/dang-ky-vip' element={<VIPRegister />} />
             </Route>
-            <Route element={<AuthenStaff />}>
+            <Route>
                 <Route path='/tao-bai-tuyen-dung' element={<RecruimentPostHR />} />
                 <Route path='/chinh-sua-bai-tuyen-dung/:pid' element={<EditPostHR />} />
                 <Route path='/danh-sach-cong-viec' element={<JobListHR />} />
-                <Route path='/danh-sach-ung-vien' element={<CandidatesListHR />} />
-                <Route path='/lien-he-voi-ung-vien' element={<ChatWithHR type={2} />} />
-                <Route path='/xem-cv-ung-vien/:path/:name' element={<ViewCV />} />
+                <Route path='/danh-sach-ung-vien' element={<CandidatesListHR />} /> />
             </Route>
-            <Route element={<AuthenHRManager />}>
+            <Route>
                 <Route path='/danh-sach-ung-vien-hrmanager' element={<HRManagerCandidatesList />} />
                 <Route path='/danh-sach-cong-viec-hrmanager' element={<JobListHRManager />} />
             </Route>
